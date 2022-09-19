d5daaeeae5c2b417fa06348a86efde6800d2a2fd
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-18 19:30:43 -0500

    ♻️🌌
    +

--- plugins/google-img.js
+++ plugins/google-img.js
@@ -3,7 +3,8 @@
 **/
 import * as fs from 'fs'
 
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn, text, args }) => {
+	if (!args[0]) return m.reply(`Que desea buscar en Google imágenes?`)
 	let name = await conn.getName(m.sender)
 	let enc = encodeURIComponent(text)
     const msj = m.reply(MultiNK.Bsqd(name))
@@ -23,3 +24,4 @@ handler.command = /^(imagen)$/i
 handler.limit = true
 
 export default handler
+
