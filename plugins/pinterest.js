3d01c874f05846181e22147ce7e5a507f3e9c2d4
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-18 19:59:31 -0500

    ♻️🌌
    +

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -3,7 +3,8 @@
 **/
 import * as fs from 'fs'
 
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn, text, args }) => {
+	if (!args[0]) return m.reply(`Que desea buscar en pinterest?`)
 	let name = await conn.getName(m.sender)
 	let enc = encodeURIComponent(text)
     const msj = m.reply(MultiNK.Bsqd(name))
