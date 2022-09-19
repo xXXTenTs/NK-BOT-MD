a74360fe58aa26082d9937cb641541e29462008b
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-18 21:01:58 -0500

    ♻️🌌
    +-

--- plugins/ytdlv-bochil.js
+++ plugins/ytdlv-bochil.js
@@ -2,7 +2,7 @@ import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, args }) => {
 	if (!args[0]) return m.reply(`Que desea descargar de Youtube?, Ejemplo de uso: \n\n${Prefijo + command} https://youtu.be/ed-6VSF-GGc`)
-	if(!isUrl(args[0]) && !args[0].includes('youtu')) return m.reply('*[ ! ] Link inválido*\n_Por favor, use un link de YouTube_\n')
+	if(!isUrl(args[0]) && !args[0].includes('youtu')) return reply('*[ ! ] Link inválido*\n_Por favor, use un link de YouTube_\n')
 	let name = await conn.getName(m.sender)
 	let mcarga = m.reply(MultiNK.Proces(name))
 	await mcarga 
@@ -15,7 +15,7 @@ let handler = async (m, { conn, args }) => {
 		const titulodl = await yt.title
 		const size = await yt.video[q].fileSizeH 
 		if (size.split('MB')[0] >= 50.00) return m.reply('[ ! ] Tu wea pesa demasiado, sorry mi king no podre enviarlo :v')
-		conn.sendMessage(m.chat, { video: {url: dl_url}, fileName: `${titulodl}.mp4`, mimetype: 'video/mp4', caption: `Titulo: ${titulodl}\nTamaño: ${size}\nExtencion: .mp4`}, { quoted: m }) 
+		await conn.sendMessage(m.chat, { video: {url: dl_url}, fileName: `${titulodl}.mp4`, mimetype: 'video/mp4', caption: `Titulo: ${titulodl}\nTamaño: ${size}\nExtencion: .mp4`}, { quoted: m }) 
 		} catch {
 			await conn.reply(m.chat, MultiNK.Error1(), m)
 }
