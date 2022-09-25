1fc0977e0d6535288353407c72506d7de8fa553d
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-24 21:51:44 -0500

    ♻️😸
    Minificación ÙwÚ

--- plugins/google-img.js
+++ plugins/google-img.js
@@ -1,27 +1,6 @@
 /**
 [ By @NeKosmic || https://github.com/NeKosmic/ ]
 **/
-import * as fs from 'fs'
-
-let handler = async (m, { conn, text, args }) => {
-	if (!args[0]) return m.reply(`Que desea buscar en Google imágenes?`)
-	let name = await conn.getName(m.sender)
-	let enc = encodeURIComponent(text)
-    const msj = m.reply(MultiNK.Bsqd(name))
-	await msj
-try {
-let gimg = await fetchJson(`https://latam-api.vercel.app/api/g_img?apikey=${MyApiKey}&q=${enc}`)
-if (!gimg.imagen) return m.reply(`[ ! ] Sin resultados`)
-conn.sendMessage(m.chat,{image:{url: gimg.imagen},caption:`*[ GOOGLE-CHAN ] ✓*\n*~> Resultado para* : _${text}_\n*~> Tamaño* : _${gimg.ancho} x ${gimg.altura} PX_`,mentions:[m.sender]},{quoted:{key:{fromMe:!1,"participant":"0@s.whatsapp.net","remoteJid":"51995386439-1604595598@g.us"},"message":{orderMessage:{itemCount:737,status:200,thumbnail:fs.readFileSync('./multimedia/imagenes/mylogo.jpg'),surface:200,message:`${name} => ${text}`,orderTitle:'Matt_M',sellerJid:'0@s.whatsapp.net'}}}})
-} catch (e) {
-m.reply(MultiNK.Error0())
-}
-}
-
-handler.help = ['imagen'].map(v => v + ' <busqueda>')
-handler.tags = ['busqueda']
-handler.command = /^(imagen)$/i
-handler.limit = true
-
-export default handler
-
+import*as e from"fs";let handler=async(a,{conn:t,text:r,args:s})=>{if(!s[0])return a.reply(`Que desea buscar en Google im\xe1genes?`);let i=await t.getName(a.sender),n=encodeURIComponent(r),l=a.reply(MultiNK.Bsqd(i));await l;try{let m=await fetchJson(`https://latam-api.vercel.app/api/g_img?apikey=${MyApiKey}&q=${n}`);if(!m.imagen)return a.reply("[ ! ] Sin resultados");t.sendMessage(a.chat,{image:{url:m.imagen},caption:`*[ GOOGLE-CHAN ] ✓*
+*~> Resultado para* : _${r}_
+*~> Tama\xf1o* : _${m.ancho} x ${m.altura} PX_`,mentions:[a.sender]},{quoted:{key:{fromMe:!1,participant:"0@s.whatsapp.net",remoteJid:"51995386439-1604595598@g.us"},message:{orderMessage:{itemCount:737,status:200,thumbnail:e.readFileSync("./multimedia/imagenes/mylogo.jpg"),surface:200,message:`${i} => ${r}`,orderTitle:"Matt_M",sellerJid:"0@s.whatsapp.net"}}}})}catch(d){a.reply(MultiNK.Error0())}};handler.help=["imagen"].map(e=>e+" <busqueda>"),handler.tags=["busqueda"],handler.command=/^(imagen)$/i,handler.limit=!0;export default handler;
