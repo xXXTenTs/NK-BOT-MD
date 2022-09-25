4847b9be3f9784a3e95399018c83013b70ba2b30
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-24 20:58:39 -0500

    ♻️😸
    Minificación ÙwÚ

--- plugins/sticker-T_L.js
+++ plugins/sticker-T_L.js
@@ -1,28 +1,31 @@
-import { sticker } from '../lib/sticker.js'
-import { stickerLine, stickerTelegram } from '@bochilteam/scraper'
+/**
+[ By @NeKosmic || https://github.com/NeKosmic/ ]
+**/
+import{sticker as e}from"../lib/sticker.js";import{stickerLine as n,stickerTelegram as t}from"@bochilteam/scraper";let handler=async(r,{conn:a,args:i,usedPrefix:l,command:s})=>{let c=/tele/i.test(s);if(!i[0])return r.reply(`*Este comando es para obtener stickers de ${c?"Telegram":"Line"}*
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    // TODO: add stickerly
-    const isTele = /tele/i.test(command)
-    if (!args[0]) return m.reply(`*Este comando es para obtener stickers de ${isTele ? 'Telegram' : 'Line'}*\n\nEjemplo de uso:\n${usedPrefix + command} anime`)
-    const json = await (isTele ? stickerTelegram : stickerLine)(args[0])
-    m.reply(`
-*Resultados encontrados para ~${args[0]}~, total:* ${(json[0]?.stickers || json).length}
+Ejemplo de uso:
+${l+s} anime`);let o=await (c?t:n)(i[0]);for(let d of(r.reply(`
+*Resultados encontrados para ~${i[0]}~, total:* ${(o[0]?.stickers||o).length}
 _Enviando stickers..._
-`.trim())
-    for (let data of (json[0]?.stickers || json)) {
-        const stiker = await sticker(false, data.sticker || data, ``, `\n< ${NombreDelBot} >\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n[ NeKosmic ]`)
-        await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m).catch(console.error)
-        await delay(1500)
-    }
+`.trim()),o[0]?.stickers||o)){let m=await e(!1,d.sticker||d,"",`
+< ${NombreDelBot} >
 
-}
-handler.help = ['telesticker <buscar>', 'linesticker <buscar>']
-handler.tags = ['conversor']
-handler.command = /^(telesticker)|(linesticker)$/i
 
-handler.limit = 5
 
-export default handler
 
-const delay = time => new Promise(res => setTimeout(res, time))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+[ NeKosmic ]`);await a.sendFile(r.chat,m,"sticker.webp","",r).catch(console.error),await delay(1500)}};handler.help=["telesticker <buscar>","linesticker <buscar>"],handler.tags=["conversor"],handler.command=/^(telesticker)|(linesticker)$/i,handler.limit=5;export default handler;let delay=e=>new Promise(n=>setTimeout(n,e));
