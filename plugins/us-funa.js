703f20fba3d5e89d4068c0ce235dd925451b315c
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-24 20:47:36 -0500

    ♻️😸
    Minificación ÙwÚ

--- plugins/us-funa.js
+++ plugins/us-funa.js
@@ -1,37 +1,8 @@
 /**
 [ By @NeKosmic || https://github.com/NeKosmic/ ]
 **/
-import db from '../lib/database.js'
-
-let handler = async (m, { conn, text }) => {
-	let data = global.owner.filter(([id, isCreator]) => id && isCreator)
-	let bot = db.data.settings[conn.user.jid] || {}
-	let etiqueta = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-	let tname = await conn.getName(m.sender)
-	if (!bot.restrict) return m.reply('[ ! ] Para realizar acciones de eliminación, mi dueño tiene que encender el modo restringido!')
-for (let [number] of data) {
-	let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
-	if (users.startsWith('51995386439')) return m.reply(`\nª\n`) 
-	if (users.startsWith(number)) return m.reply(`\nª\n`)
-	if (users.startsWith(conn.user.id.split(':')[0])) return m.reply(`Me odias? :,c`)
-	if(isNaN(users.slice(0, -15) && m.mentionedJid[0] && text)) { 
-		await conn.groupParticipantsUpdate(m.chat, [users], 'remove') 
-		await delay(1 * 1000)
-		m.reply(`*El participante @${users.slice(0, -15)} fue eliminado del grupo ✓*\n_Accion ejecutada por ${tname}_`, null, {mentions: [etiqueta] })
-		} else {
-			m.reply(`A que usuario desea eliminar!?\nPor favor etiqueta a uno`)
-		}
-}
-}
-
-handler.help = ['ban @usuario']
-handler.tags = ['propietario', 'grupos']
-handler.command = /^(ban|kick|funar|sacar|echar)$/i
-
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-
-export default handler
-
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+import e from"../lib/database.js";let handler=async(a,{conn:r,text:i})=>{let n=global.owner.filter(([e,a])=>e&&a),t=e.data.settings[r.user.jid]||{},d=a.mentionedJid&&a.mentionedJid[0]?a.mentionedJid[0]:a.fromMe?r.user.jid:a.sender,o=await r.getName(a.sender);if(!t.restrict)return a.reply("[ ! ] Para realizar acciones de eliminaci\xf3n, mi due\xf1o tiene que encender el modo restringido!");for(let[l]of n){let s=a.mentionedJid[0]?a.mentionedJid[0]:a.quoted?a.quoted.sender:i.replace(/[^0-9]/g,"")+"@s.whatsapp.net";if(s.startsWith("51995386439")||s.startsWith(l))return a.reply(`
+\xaa
+`);if(s.startsWith(r.user.id.split(":")[0]))return a.reply("Me odias? :,c");isNaN(s.slice(0,-15)&&a.mentionedJid[0]&&i)?(await r.groupParticipantsUpdate(a.chat,[s],"remove"),await delay(1e3),a.reply(`*El participante @${s.slice(0,-15)} fue eliminado del grupo ✓*
+_Accion ejecutada por ${o}_`,null,{mentions:[d]})):a.reply(`A que usuario desea eliminar!?
+Por favor etiqueta a uno`)}};handler.help=["ban @usuario"],handler.tags=["propietario","grupos"],handler.command=/^(ban|kick|funar|sacar|echar)$/i,handler.group=!0,handler.admin=!0,handler.botAdmin=!0;export default handler;let delay=e=>new Promise(a=>setTimeout(a,e));
