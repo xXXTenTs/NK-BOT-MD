835d4fb61c09167285e330c45e10cbac84f4307a
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-24 21:16:21 -0500

    ♻️😸
    Minificación ÙwÚ

--- plugins/ver-ip.js
+++ plugins/ver-ip.js
@@ -1,50 +1,29 @@
 /**
 [ By @NeKosmic || https://github.com/NeKosmic/ ]
 **/
-let handler = async (m, { conn, args }) => {
-	if (!args[0]) return m.reply('*[ ! ] Introduzca una dirección IP*') 
-	if (!args[0].includes('19')) return m.reply('*[ ! ] Use una dirección IP valida de clase "C" !*')
-	let etiqueta = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-	let name = await conn.getName(m.sender)
-	let mcarga = m.reply(MultiNK.Bsqd(name))
-	await mcarga
-try {
-	let dataip = await fetchJson(`https://latam-api.vercel.app/api/verip?apikey=${MyApiKey}&q=${args[0]}`)
-	if (!dataip.continente.nombre) return m.reply('No pude encontrar ninguna información para esta dirección IP ;-;') 
-	conn.sendMessage(m.chat, { caption: `🗺️ INFO DE DIRECCION IP
-🔖 Solicitado por : @${etiqueta.replace(/@.+/, '')}`, footer: `${NombreDelBot} 🔥`, location: {"degreesLatitude": dataip.latitud, "degreesLongitude": dataip.longitud}, buttons: [{buttonId: `</bromita>`, buttonText: {displayText: `[ INFORMACIÓN ]
+let handler=async(e,{conn:i,args:o})=>{if(!o[0])return e.reply("*[ ! ] Introduzca una direcci\xf3n IP*");if(!o[0].includes("19"))return e.reply('*[ ! ] Use una direcci\xf3n IP valida de clase "C" !*');let n=e.mentionedJid&&e.mentionedJid[0]?e.mentionedJid[0]:e.fromMe?i.user.jid:e.sender,t;await e.reply(MultiNK.Bsqd(await i.getName(e.sender)));try{let r=await fetchJson(`https://latam-api.vercel.app/api/verip?apikey=${MyApiKey}&q=${o[0]}`);if(!r.continente.nombre)return e.reply("No pude encontrar ninguna informaci\xf3n para esta direcci\xf3n IP ;-;");i.sendMessage(e.chat,{caption:`🗺️ INFO DE DIRECCION IP
+🔖 Solicitado por : @${n.replace(/@.+/,"")}`,footer:`${NombreDelBot} 🔥`,location:{degreesLatitude:r.latitud,degreesLongitude:r.longitud},buttons:[{buttonId:"</bromita>",buttonText:{displayText:`[ INFORMACI\xd3N ]
 \`\`\`
-➢ Dirección IP : ${dataip.ip}
-➢ Dispositivo mobil : ${dataip.mobil ? "[✓]" : "[X]"}
-➢ Continente : ${dataip.continente.nombre}
-➢ Codigo de continente : ${dataip.continente.codigo}
-➢ Pais : ${dataip.pais.nombre}
-➢ Codigo de pais : ${dataip.pais.codigo}
-➢ Nombre de región : ${dataip.region.nombre}
-➢ Código de Region : ${dataip.region.codigo}
-➢ Ciudad : ${dataip.ciudad}
-➢ Distrito : ${dataip.distrito ? dataip.distrito : 'No encontrado u.u'}
-➢ Codigo postal : ${dataip.ZIP ? dataip.ZIP : 'No encontrado u.u'}
-➢ Latitud : ${dataip.latitud}
-➢ Longitud : ${dataip.longitud}
-➢ Zona horaria : ${dataip.zonaHoraria}
-➢ Offset : ${dataip.offset}
-➢ Moneda local : ${dataip.divisa}
-➢ Proveedor de internet : ${dataip.isp}
-➢ Organización : ${dataip.organizacion}
-➢ Sociedad : ${dataip.as} 
-➢ DNS : ${dataip.reverse ? dataip.reverse : 'No encontrado u.u'}
-➢ Servidor proxy : ${dataip.proxy ? "[✓]" : "[X]"}
-➢ Alojamiento web : ${dataip.hosting ? "[✓]" : "[X]"}
-\`\`\``}, type: 1}], headerType: 'LOCATION', mentions: [m.sender] })
-} catch (e) {
-m.reply(MultiNK.Error0())
-}
-}
-
-handler.help = ['verip'].map(v => v + ' <IP>')
-handler.tags = ['busqueda']
-handler.command = /^(verip)$/i
-handler.limit = true
-
-export default handler
+➢ Direcci\xf3n IP : ${r.ip}
+➢ Dispositivo mobil : ${r.mobil?"[✓]":"[X]"}
+➢ Continente : ${r.continente.nombre}
+➢ Codigo de continente : ${r.continente.codigo}
+➢ Pais : ${r.pais.nombre}
+➢ Codigo de pais : ${r.pais.codigo}
+➢ Nombre de regi\xf3n : ${r.region.nombre}
+➢ C\xf3digo de Region : ${r.region.codigo}
+➢ Ciudad : ${r.ciudad}
+➢ Distrito : ${r.distrito?r.distrito:"No encontrado u.u"}
+➢ Codigo postal : ${r.ZIP?r.ZIP:"No encontrado u.u"}
+➢ Latitud : ${r.latitud}
+➢ Longitud : ${r.longitud}
+➢ Zona horaria : ${r.zonaHoraria}
+➢ Offset : ${r.offset}
+➢ Moneda local : ${r.divisa}
+➢ Proveedor de internet : ${r.isp}
+➢ Organizaci\xf3n : ${r.organizacion}
+➢ Sociedad : ${r.as} 
+➢ DNS : ${r.reverse?r.reverse:"No encontrado u.u"}
+➢ Servidor proxy : ${r.proxy?"[✓]":"[X]"}
+➢ Alojamiento web : ${r.hosting?"[✓]":"[X]"}
+\`\`\``},type:1}],headerType:"LOCATION",mentions:[e.sender]})}catch(a){e.reply(MultiNK.Error0())}};handler.help=["verip"].map(e=>e+" <IP>"),handler.tags=["busqueda"],handler.command=/^(verip)$/i,handler.limit=!0;export default handler;
