4b3307c0c8694395c2adc94827a394f6e725b1d0
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-24 21:35:34 -0500

    ♻️😸
    Minificación ÙwÚ

--- plugins/anime-neko.js
+++ plugins/anime-neko.js
@@ -1,24 +1,5 @@
 /**
 [ By @NeKosmic || https://github.com/NeKosmic/ ]
 **/
-import * as fs from 'fs'
-
-let handler = async (m, { conn, command }) => {
-	let etiqueta = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-	let name = await conn.getName(m.sender)
-	const msj = m.reply(MultiNK.Proces(name))
-	await msj
-try {
-var dataimg = await fetchJson(`https://latam-api.vercel.app/api/nekonime1?apikey=${MyApiKey}`)
-conn.sendMessage(m.chat, { image: {url: dataimg.imagen}, caption: `┏━⊱ Imagen : ${command}\n┗⊱ Solicitada por : @${etiqueta.replace(/@.+/, '')}`, mentions: [etiqueta] }, { quoted: {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "51995386439-1604595598@g.us"}, "message": {orderMessage: {itemCount: 737,status: 200, thumbnail: fs.readFileSync('./multimedia/imagenes/mylogo.jpg'), surface: 200, message: `${name} [_>] ${command}`, orderTitle: 'Matt_M', sellerJid: '0@s.whatsapp.net'}}} })
-} catch (e) {
-m.reply(MultiNK.Error0())
-}
-}
-
-handler.help = ['neko']
-handler.tags = ['animeuwu']
-handler.command = /^(neko)$/i
-handler.limit = true
-
-export default handler
+import*as e from"fs";let handler=async(a,{conn:t,command:i})=>{let n=a.mentionedJid&&a.mentionedJid[0]?a.mentionedJid[0]:a.fromMe?t.user.jid:a.sender,r=await t.getName(a.sender),s=a.reply(MultiNK.Proces(r));await s;try{var m=await fetchJson(`https://latam-api.vercel.app/api/nekonime1?apikey=${MyApiKey}`);t.sendMessage(a.chat,{image:{url:m.imagen},caption:`┏━⊱ Imagen : ${i}
+┗⊱ Solicitada por : @${n.replace(/@.+/,"")}`,mentions:[n]},{quoted:{key:{fromMe:!1,participant:"0@s.whatsapp.net",remoteJid:"51995386439-1604595598@g.us"},message:{orderMessage:{itemCount:737,status:200,thumbnail:e.readFileSync("./multimedia/imagenes/mylogo.jpg"),surface:200,message:`${r} [_>] ${i}`,orderTitle:"Matt_M",sellerJid:"0@s.whatsapp.net"}}}})}catch(d){a.reply(MultiNK.Error0())}};handler.help=["neko"],handler.tags=["animeuwu"],handler.command=/^(neko)$/i,handler.limit=!0;export default handler;
