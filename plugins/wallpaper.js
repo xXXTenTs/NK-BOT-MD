5cc572c169236f1c250395b2e4db2743bcf27a31
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-24 21:50:10 -0500

    ♻️😸
    Minificación ÙwÚ

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -1,27 +1,5 @@
 /**
 [ By @NeKosmic || https://github.com/NeKosmic/ ]
 **/
-import * as fs from 'fs'
-
-let handler = async (m, { conn, text, command, args }) => {
-        if (!args[0]) return m.reply(`Cual es su búsqueda?`)
-	let etiqueta = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-	let name = await conn.getName(m.sender)
-	let enc = encodeURIComponent(text)
-    const msj = m.reply(MultiNK.Bsqd(name))
-	await msj
-try {
-var wpp = await fetchJson(`https://latam-api.vercel.app/api/wpp?apikey=${MyApiKey}&q=${enc}`)
-if (!wpp.imagen) return m.reply(`[ ! ] Sin resultados`)
-conn.sendMessage(m.chat,{image:{url: wpp.imagen},caption:`┏━⊱ Wallpaper : ${text}\n┗⊱  Solicitada por : @${etiqueta.replace(/@.+/, '')}`,mentions:[m.sender]},{quoted:{key:{fromMe:!1,"participant":"0@s.whatsapp.net","remoteJid":"51995386439-1604595598@g.us"},"message":{orderMessage:{itemCount:737,status:200,thumbnail:fs.readFileSync('./multimedia/imagenes/mylogo.jpg'),surface:200,message:`${name} => ${text}`,orderTitle:'Matt_M',sellerJid:'0@s.whatsapp.net'}}}})
-} catch (e) {
-m.reply(MultiNK.Error0())
-}
-}
-
-handler.help = ['wallpaper'].map(v => v + ' <busqueda>')
-handler.tags = ['busqueda']
-handler.command = /^(wallpaper|wpp|fondo)$/i
-handler.limit = true
-
-export default handler
+import*as e from"fs";let handler=async(a,{conn:r,text:t,command:s,args:l})=>{if(!l[0])return a.reply(`Cual es su b\xfasqueda?`);let p=await r.getName(a.sender),i=encodeURIComponent(t),n=a.reply(MultiNK.Bsqd(p));await n;try{var d=await fetchJson(`https://latam-api.vercel.app/api/wpp?apikey=${MyApiKey}&q=${i}`);if(!d.imagen)return a.reply("[ ! ] Sin resultados");r.sendMessage(a.chat,{image:{url:d.imagen},caption:`┏━⊱ Wallpaper : ${t}
+┗⊱  Solicitada por : @${a.sender.replace(/@.+/,"")}`,mentions:[a.sender]},{quoted:{key:{fromMe:!1,participant:"0@s.whatsapp.net",remoteJid:"51995386439-1604595598@g.us"},message:{orderMessage:{itemCount:737,status:200,thumbnail:e.readFileSync("./multimedia/imagenes/mylogo.jpg"),surface:200,message:`${p} => ${t}`,orderTitle:"Matt_M",sellerJid:"0@s.whatsapp.net"}}}})}catch(m){a.reply(MultiNK.Error0())}};handler.help=["wallpaper"].map(e=>e+" <busqueda>"),handler.tags=["busqueda"],handler.command=/^(wallpaper|wpp|fondo)$/i,handler.limit=!0;export default handler;
