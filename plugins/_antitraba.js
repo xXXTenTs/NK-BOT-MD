720195367677b2f28127396e63bb70f194c1901a
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-24 20:49:20 -0500

    ♻️😸
    Minificación ÙwÚ

--- plugins/_antitraba.js
+++ plugins/_antitraba.js
@@ -1,31 +1,9 @@
 /**
 [ By @NeKosmic || https://github.com/NeKosmic/ ]
 **/
-import * as fs from 'fs'
-import db from '../lib/database.js'
-
-export async function before(m, { conn, isAdmin, isBotAdmin, usedPrefix }) {
-    if (m.isBaileys && m.fromMe)
-        return !0
-    if (!m.isGroup) return !1
-    let chat = db.data.chats[m.chat]
-    let bot = db.data.settings[this.user.jid] || {}
-    let prt = m.key.participant
-    let yid = m.key.id
-    let name = await conn.getName(m.sender)
-    let fakemek = {key: {participant: "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "51995386439-1616969743@g.us","inviteCode": "m","groupName": "P", "caption": NombreDelBot, 'jpegThumbnail': fs.readFileSync('./multimedia/imagenes/mylogo.jpg')}}}
-    if (chat.antiTraba && m.text.length > 1200) {
-    	if (isAdmin) return conn.sendMessage(m.chat, { text: `El administrador @${m.sender.split("@")[0]} acaba de enviar un texto que contiene muchos caracteres -.-!`, mentions: [m.sender] }, { quoted: fakemek })
-    await conn.sendButton(m.chat, `*[ ! ] Se detecto un mensaje que contiene muchos caracteres [ ! ]*\n`, `${isBotAdmin ? '' : 'No soy administrador, no puedo hacer nada :/'}`, NombreDelBot, ['[ DESACTIVAR ANTI TRABAS ]', usedPrefix+'apagar antitraba'], fakemek )
-        if (isBotAdmin && bot.restrict) {
-        	conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: yid, participant: prt }})
-        setTimeout(() => { 
-        	conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-            }, 500)
-        	setTimeout(() => { 
-        	conn.sendMessage(m.chat, { text: `Marcar el chat como leido ✓\n${"\n".repeat(400)}\n=> El número : wa.me/${m.sender.split("@")[0]}\n=> Alias : ${name}\n[ ! ] Acaba de enviar un texto que contiene muchos caracteres que puede ocasionar fallos en los dispositivos`, mentions: [m.sender] }, { quoted: fakemek })
-        }, 1000)
-        } else if (!bot.restrict) return m.reply('[ ! ] Para realizar acciones de eliminación, mi dueño tiene que encender el modo restringido!')
-    }
-    return !0
-}
+import*as e from"fs";import a from"../lib/database.js";export async function before(t,{conn:i,isAdmin:n,isBotAdmin:r,usedPrefix:s}){if(t.isBaileys&&t.fromMe)return!0;if(!t.isGroup)return!1;let o=a.data.chats[t.chat],d=a.data.settings[this.user.jid]||{},c=t.key.participant,m=t.key.id,u=await i.getName(t.sender),p={key:{participant:"0@s.whatsapp.net",remoteJid:"0@s.whatsapp.net"},message:{groupInviteMessage:{groupJid:"51995386439-1616969743@g.us",inviteCode:"m",groupName:"P",caption:NombreDelBot,jpegThumbnail:e.readFileSync("./multimedia/imagenes/mylogo.jpg")}}};if(o.antiTraba&&t.text.length>1200){if(n)return i.sendMessage(t.chat,{text:`El administrador @${t.sender.split("@")[0]} acaba de enviar un texto que contiene muchos caracteres -.-!`,mentions:[t.sender]},{quoted:p});if(await i.sendButton(t.chat,`*[ ! ] Se detecto un mensaje que contiene muchos caracteres [ ! ]*
+`,`${r?"":"No soy administrador, no puedo hacer nada :/"}`,NombreDelBot,["[ DESACTIVAR ANTI TRABAS ]",s+"apagar antitraba"],p),r&&d.restrict)i.sendMessage(t.chat,{delete:{remoteJid:t.chat,fromMe:!1,id:m,participant:c}}),setTimeout(()=>{i.groupParticipantsUpdate(t.chat,[t.sender],"remove")},500),setTimeout(()=>{i.sendMessage(t.chat,{text:`Marcar el chat como leido ✓
+${"\n".repeat(400)}
+=> El n\xfamero : wa.me/${t.sender.split("@")[0]}
+=> Alias : ${u}
+[ ! ] Acaba de enviar un texto que contiene muchos caracteres que puede ocasionar fallos en los dispositivos`,mentions:[t.sender]},{quoted:p})},1e3);else if(!d.restrict)return t.reply("[ ! ] Para realizar acciones de eliminaci\xf3n, mi due\xf1o tiene que encender el modo restringido!")}return!0}
