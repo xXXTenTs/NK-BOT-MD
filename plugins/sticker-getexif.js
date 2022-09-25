3ae9ecba55242076b1be447147b0a8da3c9a70a6
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-24 20:43:36 -0500

    ♻️😸
    Minificación ÙwÚ

--- plugins/sticker-getexif.js
+++ plugins/sticker-getexif.js
@@ -1,18 +1,1 @@
-import { format } from 'util'
-const { default: { Image } } = await import('node-webpmux')
-
-let handler = async (m) => {
-    if (!m.quoted) return m.reply('[ ! ] Responda un sticker!')
-    if (/sticker/.test(m.quoted.mtype)) {
-        let img = new Image()
-        await img.load(await m.quoted.download())
-        m.reply(format(JSON.parse(img.exif.slice(22).toString())))
-    }
-}
-
-handler.help = ['getexif']
-handler.tags = ['conversor']
-
-handler.command = ['getexif']
-
-export default handler
+import{format as e}from"util";let{default:{Image:t}}=await import("node-webpmux"),handler=async a=>{if(!a.quoted)return a.reply("[ ! ] Responda un sticker!");if(/sticker/.test(a.quoted.mtype)){let r=new t;await r.load(await a.quoted.download()),a.reply(e(JSON.parse(r.exif.slice(22).toString())))}};handler.help=["getexif"],handler.tags=["conversor"],handler.command=["getexif"];export default handler;
