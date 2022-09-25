7dc8f2eb02f197849540a596ad3a9ea4eadf4b82
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-24 21:04:51 -0500

    ♻️😸
    Minificación ÙwÚ

--- plugins/yt-busqueda.js
+++ plugins/yt-busqueda.js
@@ -1,27 +1,21 @@
 /**
 [ By @NeKosmic || https://github.com/NeKosmic/ ]
 **/
-let handler = async (m, { conn, text, command }) => {
-	if (!text) return m.reply(`Que desea buscar en Youtube?, Ejemplo de uso: \n\n${Prefijo + command} cuanto es 1 + 1`)
-	let name = await conn.getName(m.sender)
-	let mcarga = m.reply(MultiNK.Bsqd(name))
-	await mcarga
-	let ytstext = encodeURIComponent(text)
-try {
-	let myapiyts = await fetchJson(`https://latam-api.vercel.app/api/yts?apikey=${MyApiKey}&q=${ytstext}`)
-	let teks = '*[ > ] Resultados en Youtube para:* _'+text+'_\n\n'
-	let no = 1
-	for (let i of myapiyts.resultados) {
-		teks += `🔖 Titulo: ${i.titulo}\n🧬 ID: ${i.id}\n⛓️ URL: ${i.url}\n🗜️ Tipo: ${i.tipo}\n🖼️ Miniatura: ${i.imagen}\n⌚ Duracion: ${i.duracion}\n📜 Descripción: ${i.descripcion}\n📆 Fecha de subida: ${i.f_carga}\n👀 Vistas: ${i.vistas}\n||\n⚡Autor: ${i.autor}\n📺 Canal: ${i.canal}\n\n*——————————*\n\n`
-	}
-m.reply(teks)
-} catch (e) {
-m.reply(MultiNK.Error0())
-}
-}
+let handler=async(a,{conn:e,text:n,command:t})=>{if(!n)return a.reply(`Que desea buscar en Youtube?, Ejemplo de uso: 
 
-handler.help = ['ytbuscar <texto>']
-handler.tags = ['busqueda']
-handler.command = /^(ytbuscar|ytsearch|yts)$/i
+${Prefijo+t} cuanto es 1 + 1 :v`);let r;await a.reply(MultiNK.Bsqd(await e.getName(a.sender)));let l=encodeURIComponent(n);try{let s=await fetchJson(`https://latam-api.vercel.app/api/yts?apikey=${MyApiKey}&q=${l}`),i="*[ > ] Resultados en Youtube para:* _"+n+"_\n\n";for(let u of s.resultados)i+=`🔖 Titulo: ${u.titulo}
+🧬 ID: ${u.id}
+⛓️ URL: ${u.url}
+🗜️ Tipo: ${u.tipo}
+🖼️ Miniatura: ${u.imagen}
+⌚ Duracion: ${u.duracion}
+📜 Descripci\xf3n: ${u.descripcion}
+📆 Fecha de subida: ${u.f_carga}
+👀 Vistas: ${u.vistas}
+||
+⚡Autor: ${u.autor}
+📺 Canal: ${u.canal}
 
-export default handler
+*——————————*
+
+`;a.reply(i)}catch(o){a.reply(MultiNK.Error0())}};handler.help=["ytbuscar <texto>"],handler.tags=["busqueda"],handler.command=/^(ytbuscar|ytsearch|yts)$/i;export default handler;
