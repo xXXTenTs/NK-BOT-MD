1565bfee99c3ae1fb916a21aa26b50719cf4a1f3
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-25 14:34:56 -0500

    ♻️🌌
    +

--- plugins/juego-pareja.js
+++ plugins/juego-pareja.js
@@ -1,9 +1,9 @@
 /**
 [ By @NeKosmic || https://github.com/NeKosmic/ ]
 **/
-import*as e from"fs";let handler=async(a,{conn:n,participants:i})=>{let t,l=`❥ *Pareja formada* : 
+import*as e from"fs";let handler=async(a,{conn:i,participants:n})=>{let t,l=`❥ *Pareja formada* : 
 
  ┏─━─━─━∞◆∞━─━─━─┓
-@${i[Math.floor(Math.random()*i.length)].id.split("@")[0]} ❤️ @${i[Math.floor(Math.random()*i.length)].id.split("@")[0]} 
+@${n[Math.floor(Math.random()*n.length)].id.split("@")[0]} ❤️ @${n[Math.floor(Math.random()*n.length)].id.split("@")[0]} 
  ┗─━─━─━∞◆∞━─━─━─┛
-`;setTimeout(()=>{a.reply(l,a.chat,{mentions:n.parseMention(l)})},500),await n.sendMessage(a.chat,{audio:e.readFileSync("./multimedia/sonidos/willy.m4a"),fileName:"NoviosXD.mp3",mimetype:"audio/mpeg",ptt:!0},{quoted:{key:{participant:"0@s.whatsapp.net"},message:{locationMessage:{name:`Esto fue posible gracias a ${await n.getName(a.sender)}`,jpegThumbnail:e.readFileSync("./multimedia/imagenes/logo.jpg")}}}})};handler.help=["emparejar"],handler.tags=["games"],handler.command=/^(emparejar)$/i;export default handler;
+`;setTimeout(()=>{a.reply(l,a.chat,{mentions:i.parseMention(l)})},500),await i.sendMessage(a.chat,{audio:e.readFileSync("./multimedia/sonidos/willy.m4a"),fileName:"NoviosXD.mp3",mimetype:"audio/mpeg",ptt:!0},{quoted:{key:{participant:"0@s.whatsapp.net"},message:{locationMessage:{name:`Esto fue posible gracias a ${await i.getName(a.sender)}`,jpegThumbnail:e.readFileSync("./multimedia/imagenes/logo.jpg")}}}})};handler.help=["emparejar"],handler.tags=["games"],handler.command=/^(emparejar)$/i,handler.limit=!0;export default handler;
