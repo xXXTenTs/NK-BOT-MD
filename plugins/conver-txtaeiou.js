214234c7f367ddbb00e569ba2b23d96c150d15c0
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-24 22:41:38 -0500

    ✍️🌌
    :v

---
+++ plugins/conver-txtaeiou.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn, command, text }) => {
+     if (!m.quoted && !text) return m.reply(`Envie o responda un texto, ejemplo de uso:\n\n${Prefijo + command} lenguaje inclusivo\n`)
+try {
+var ter = command[1].toLowerCase()
+var tex = m.quoted ? m.quoted.text : text
+await conn.sendMessage(m.chat,{text:tex.replace(/[aiueo]/g, ter).replace(/[AIUEO]/g, ter.toUpperCase())}, {quoted:m})
+reacMoji(m.chat,conn,'💅',m)
+} catch (e) {
+m.reply(MultiNK.Error1())
+}
+}
+
+handler.help = ['taxta | texte | tixti | toxto | tuxtu']
+handler.tags = ['conversor']
+handler.command = /^(taxta|texte|tixti|toxto|tuxtu)$/i
+
+export default handler
