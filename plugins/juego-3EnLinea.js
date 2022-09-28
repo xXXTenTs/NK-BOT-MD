e36e44d338e2457033809b89c304d9f9140890da
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-27 20:17:01 -0500

    ✍️🌌
    +

---
+++ plugins/juego-3EnLinea.js
@@ -0,0 +1,64 @@
+import TicTacToe from '../lib/tictactoe.js'
+
+let handler = async (m, { conn, command, text }) => {
+    conn.game = conn.game ? conn.game : {}
+    if (Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) return m.reply('Todavía estás en el juego -.-')
+    let room = Object.values(conn.game).find(room => room.state === 'ESPERE' && (text ? room.name === text : true))
+    // m.reply('[WIP Feature]')
+    if (room) {
+        m.reply('Rival encontrado!')
+        room.o = m.chat
+        room.game.playerO = m.sender
+        room.state = 'JUGANDO'
+        let arr = room.game.render().map(v => {
+            return {
+                X: '❌',
+                O: '⭕',
+                1: '1️⃣',
+                2: '2️⃣',
+                3: '3️⃣',
+                4: '4️⃣',
+                5: '5️⃣',
+                6: '6️⃣',
+                7: '7️⃣',
+                8: '8️⃣',
+                9: '9️⃣',
+            }[v]
+        })
+        let str = `
+ID de sala: ${room.id}
+
+${arr.slice(0, 3).join('')}
+${arr.slice(3, 6).join('')}
+${arr.slice(6).join('')}
+
+@${room.game.currentTurn.split('@')[0]} tiene que comenzar el juego
+
+~Escriba~ *rendirse* ~para darse por vencido~
+`.trim()
+        if (room.x !== room.o) await conn.sendButton(room.x, str, NombreDelBot, ['RENDIRSE', 'rendirse'], m, {
+            mentions: conn.parseMention(str)
+        })
+        await conn.sendButton(room.o, str, NombreDelBot, ['RENDIRSE', 'rendirse'], m, {
+            mentions: conn.parseMention(str)
+        })
+    } else {
+        room = {
+            id: 'tictactoe-' + (+new Date),
+            x: m.chat,
+            o: '',
+            game: new TicTacToe(m.sender, 'o'),
+            state: 'ESPERE'
+        }
+        if (text) room.name = text
+        m.reply('*[ ! ]Esperando rival*\n' + (text ? ` Para unirse al juego use el comando:
+\n\n${Prefijo + command} ${text}\n` : ` Para unirse al juego use el comando:\n\n ${Prefijo + command}\n\n_Recompensa para el ganador +4000 de XP_\n`))
+        conn.game[room.id] = room
+    }
+}
+
+handler.help = ['ttt'].map(v => v + ' [nombre de sala]')
+handler.tags = ['games']
+handler.command = /^(ttt)$/
+
+export default handler
