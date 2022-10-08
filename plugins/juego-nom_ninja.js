a52463721a5c73e3450b82a622398e0a956e321b
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-10-08 07:59:06 -0500

    ♻️🌌
    A

--- plugins/juego-nom_ninja.js
+++ plugins/juego-nom_ninja.js
@@ -1,11 +1,12 @@
-function handler(m, { text }) {
-    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+function handler(m, { text, args }) {
+	if (!args[0]) return m.reply(`Use un nombre o apodo`)
     m.reply(`
-☯️ Nombre ninja para _${text}_ es: *`+teks.replace(/[a-z]/gi,i=>({a:"ka",b:"tu",c:"mi",d:"te",e:"ku",f:"lu",g:"ji",h:"ri",i:"ki",j:"zu",k:"me",l:"ta",m:"rin",n:"to",o:"mo",p:"no",q:"ke",r:"shi",s:"ari",t:"ci",u:"do",v:"ru",w:"mei",x:"na",y:"fu",z:"zi"})[i.toLowerCase()]||i)+"*\n");
+☯️ Nombre ninja para _${text}_ es: *`+text.replace(/[a-z]/gi,i=>({a:"ka",b:"tu",c:"mi",d:"te",e:"ku",f:"lu",g:"ji",h:"ri",i:"ki",j:"zu",k:"me",l:"ta",m:"rin",n:"to",o:"mo",p:"no",q:"ke",r:"shi",s:"ari",t:"ci",u:"do",v:"ru",w:"mei",x:"na",y:"fu",z:"zi"})[i.toLowerCase()]||i)+"*\n");
 }
 
 handler.help = ['nombreninja <nombre>']
 handler.tags = ['games']
 handler.command =  /^(nombreninja)$/i
+handler.group = true
 
 export default handler
