5e3cd2b09882256e41716cfbe16d00710913f59a
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-10-08 07:54:49 -0500

    ♻️🌌
    A

--- plugins/juego-calumnia.js
+++ plugins/juego-calumnia.js
@@ -1,9 +1,9 @@
 /**
 [ By @NeKosmic || https://github.com/NeKosmic/ ]
 **/
-let handler=async(e,{conn:a,text:t,command:n})=>{if(!t)return e.reply(`*[ ! ] Use bien el comando :*
-${Prefijo+n} @tag|mensaje|respuesta`);if(!t.includes("|"))return e.reply(`*[ ! ] Agrege barras entre cada palabra todo junto*
+let handler=async(e,{conn:a,text:n,command:t})=>{if(!n)return e.reply(`*[ ! ] Use bien el comando :*
+${Prefijo+t} @tag|mensaje|respuesta`);if(!n.includes("|"))return e.reply(`*[ ! ] Agrege barras entre cada palabra todo junto*
 
 _Ejemplo de uso_ : 
-${Prefijo+n} @usuarioetiquetado|bendiceme|bendecido
-~No olvide usar esta barra~ : " | "`);let r=e.mentionedJid&&e.mentionedJid[0]?e.mentionedJid[0]:e.fromMe?a.user.jid:e.sender,s;await a.sendMessage(e.chat,{text:t.split("|")[2]?t.split("|")[2]:"@"+e.sender.split("@")[0]+" use 2 barras\n@tag|mensaje|respuesta ✓"},{quoted:{key:{fromMe:!1,participant:r,...e.chat?{remoteJid:e.chat}:{}},message:{conversation:t.split("|")[1]?t.split("|")[1]:"No hay texto :v"}}}),reacMoji(e.chat,a,"\uD83E\uDD2B",e)};handler.help=["calumnia"],handler.tags=["games"],handler.command=/^(calumnia)$/i;export default handler;
+${Prefijo+t} @usuarioetiquetado|bendiceme|bendecido
+~No olvide usar esta barra~ : " | "`);let r=e.mentionedJid&&e.mentionedJid[0]?e.mentionedJid[0]:e.fromMe?a.user.jid:e.sender,s;await a.sendMessage(e.chat,{text:n.split("|")[2]?n.split("|")[2]:"@"+e.sender.split("@")[0]+" use 2 barras\n@tag|mensaje|respuesta ✓"},{quoted:{key:{fromMe:!1,participant:r,...e.chat?{remoteJid:e.chat}:{}},message:{conversation:n.split("|")[1]?n.split("|")[1]:"No hay texto :v"}}}),reacMoji(e.chat,a,"\uD83E\uDD2B",e)};handler.help=["calumnia"],handler.tags=["games"],handler.command=/^(calumnia)$/i,handler.group=!0;export default handler;
